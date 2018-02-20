6376e6ca1abe62ed08f1ea0229e6f4d7ec5d3bcd
Aren <alex13gamerz@gmail.com>
2018-02-20 13:54:50 +0100

    Version 0.1
    - Finally! Promotions are here bro!

--- website/default-web.php
+++ website/default-web.php
@@ -10,19 +10,30 @@
     <?php
 
         $promotions_file = "promotions.txt";
-        $lines = file($promotions_file);
-        $last_week = (int) $lines[3];
-        $current_week = (int) date('W');
-        $promotions = (string) $lines[6];
+        $lines = file($promotions_file,  FILE_IGNORE_NEW_LINES);
+        $last_week = (string) $lines[1];
+        $current_week = (string) date('W');
 
-        // if ($current_week == $last_week) {
-        //     echo "It works :D";
-        // }
-        // else {
-        //     echo "$current_week";
-        //     echo "$last_week";
-        // }
+        if (strcmp($current_week, $last_week) != 0) {
+            $link = mysqli_connect("localhost", "root", "Abc@1234!", "pcgames");
+            $query = "SELECT id FROM games ORDER BY RAND() LIMIT 8";
+            $result = mysqli_query($link, $query);
+            $new_promotions = "";
+
+            while ($row = mysqli_fetch_array($result)) {
+                $new_promotions .= "$row[id],";
+            }
+            
+            $new_promotions = substr($new_promotions, 0, -1);
+
+            $lines[1] = $current_week;
+            $lines[4] = $new_promotions;
+            file_put_contents( $promotions_file , implode( "\n", $lines ) );
+            mysqli_close($link);
+        }
         
+        $promotions = (string) $lines[4];
+
         $max_results = 10;
         $max_rows = 2;
 
@@ -32,14 +43,15 @@
         $result = mysqli_query($link, $query);      
 
         echo "<div class='row'>";             
-        while ($query_row=mysqli_fetch_array($result)) { 
+        while ($query_row = mysqli_fetch_array($result)) { 
             echo "<div class='column'>";
             echo "<div class='title'><span>".$query_row["title"]."</span></div>";
             echo "<div class='cover'><img src='../IMG/$query_row[id]'></div>";
             echo "</div>";        
             }     
         echo "</div>";
-
+        mysqli_close($link);
+        
     ?>
 </body>
 </html>
