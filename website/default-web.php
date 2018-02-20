5bcb468048567e6219028adee4c1d449e6aeecbb
Aren <alex13gamerz@gmail.com>
2018-02-20 13:15:42 +0100

    Version 0.0.6
    - YAAAY finally I have something to show
    - Now the only things that remains to do is make the random algorithm.

--- website/default-web.php
+++ website/default-web.php
@@ -7,7 +7,39 @@
 </head>
 <body>
 
-    
+    <?php
 
+        $promotions_file = "promotions.txt";
+        $lines = file($promotions_file);
+        $last_week = (int) $lines[3];
+        $current_week = (int) date('W');
+        $promotions = (string) $lines[6];
+
+        // if ($current_week == $last_week) {
+        //     echo "It works :D";
+        // }
+        // else {
+        //     echo "$current_week";
+        //     echo "$last_week";
+        // }
+        
+        $max_results = 10;
+        $max_rows = 2;
+
+        $link = mysqli_connect("localhost", "root", "Abc@1234!", "pcgames");
+
+        $query = "SELECT id, title FROM games WHERE id IN ($promotions)";
+        $result = mysqli_query($link, $query);      
+
+        echo "<div class='row'>";             
+        while ($query_row=mysqli_fetch_array($result)) { 
+            echo "<div class='column'>";
+            echo "<div class='title'><span>".$query_row["title"]."</span></div>";
+            echo "<div class='cover'><img src='../IMG/$query_row[id]'></div>";
+            echo "</div>";        
+            }     
+        echo "</div>";
+
+    ?>
 </body>
 </html>
