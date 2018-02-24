d34c8b51315e01549ce76dda8cb1be19d28d3bdf
Aren <alex13gamerz@gmail.com>
2018-02-24 18:59:54 +0100

    Version 0.3
    - Cpanel design completed

--- website/cpanel/cpanel.php
+++ website/cpanel/cpanel.php
@@ -10,7 +10,7 @@
                 session_start();
                 if (!$_SESSION["id"]) {
                     session_destroy();
-                    echo "You're not logged in. Please <a href='../modules/login/login.html'>log in.</a>";
+                    echo "<span>You're not logged in. Please <a href='/modules/login/login.html'>log in.</a></span>";
                     echo "\n\t</body>";
                     echo "\n</html>";
                     exit;
@@ -18,7 +18,7 @@
             
                 echo "<div class='cpanel'>";
                 echo "<a href='../profile/profile.php?id=$_SESSION[id]'><div class='profile'><span>Profile<span></div></a>";
-                echo "<div class='more'><span>Stay Tuned! More features coming soon!</span></div>";
+                echo "<div class='more'><span>Coming soon!</span></div>";
                 if (strcmp($_SESSION["roles"], "admin") == 0) {
                     echo "<a href='../management/usermanagement.php'><div class='userManagement'><span>Manage Users<span></div></a>";                    
                     echo "<a href='../management/gamemanagement.php'><div class='gameManagement'><span>Manage Games<span></div></a>";                    
